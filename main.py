BH4/echo-mouse

commit 81e62a5843304695072298ae7190714a020359d9

Remove last click when used to end recording
Closes issue #4

Currently checks if time between last click is less than 0.01 seconds, but its possible this should be larger on some systems.

main.py
@@ -395,6 +395,7 @@ class MainWindow(QMainWindow):
         #     return False
 
     def record(self):
+        t = time()
         self.recording = not self.recording
         if self.recording:
             print('Starting recording')
@@ -404,6 +405,15 @@ class MainWindow(QMainWindow):
 
             self.runtime_text.setText("Runtime: Recording...")
         else:
+            # If the last click was a release and the time is very close to
+            # the recording stop, then the last 2 clicks were likely on the
+            # record button and should be removed.
+            if t-self.prev_click_time < 0.01 and not self.clicks[-1][3]:
+                self.clicks = self.clicks[:-2]
+                self.timing = self.timing[:-2]
+
+                if self.verbose:
+                    print('Final click removed as it was likely used to end recording.')
             print('Recording ended')
 
             self.calculate_runtime()

commit 05e37a9c13c9564f4209fae9ea2cfbd2dc9eda8b

Fix bug introduced in a previous commit
Commit 81e62a5843304695072298ae7190714a020359d9 introduced a check to remove the mouse button click which stops recording, but if the F1 shortcut is used and no mouse buttons are ever clicked this caused an error. Now we check if there are any clicks first.

main.py
@@ -408,7 +408,7 @@ class MainWindow(QMainWindow):
             # If the last click was a release and the time is very close to
             # the recording stop, then the last 2 clicks were likely on the
             # record button and should be removed.
-            if t-self.prev_click_time < 0.01 and not self.clicks[-1][3]:
+            if self.prev_click_time is not None and (t-self.prev_click_time < 0.01 and not self.clicks[-1][3]):
                 self.clicks = self.clicks[:-2]
                 self.timing = self.timing[:-2]
 